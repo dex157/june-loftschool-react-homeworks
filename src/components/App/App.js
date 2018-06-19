5a57684c28968c1d8287201156b830c9e7fbfd23
Artem Samofalov <dex157@gmail.com>
2018-06-19 19:04:35 +0300

    add file

---
+++ src/components/App/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+class Welcome extends React.Component {
+  render() {
+    return (
+      <div className="App">
+        <p className="description">Миру-мир, студентам - beer.</p>
+      </div>
+    );
+  }
+}
+
+export default Welcome;
