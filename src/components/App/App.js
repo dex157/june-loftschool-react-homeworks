ed56c8ae5a69de10a52c9dace86a57fbe7ef9d67
szadorozhniy <szadorozhniy@axiomsl.com>
2018-06-19 18:13:33 +0300

    homework1 done

---
+++ src/components/App/App.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+class Welcome extends React.Component {
+	render() {
+		return <div className="App">
+			<p className="description">Миру-мир, студентам - beer.</p>
+		</div>;
+	}
+}
+
+export default Welcome;
