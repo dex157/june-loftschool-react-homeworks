e572c1b8f6896dc7ef9a0cbf2ff21a9bb93dfcf4
Andrei <dswfcny7@mail.ru>
2018-06-20 07:27:02 +0300

    complete first homework

---
+++ src/components/App/App.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+export default function First() {
+    return(
+      <div className="App">
+        <p className="description">Миру-мир, студентам - beer.</p>
+      </div>
+    )
+}
