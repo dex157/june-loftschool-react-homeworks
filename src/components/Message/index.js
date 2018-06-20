261d49c0f459c1fe91e1fddd82b3cc5ae6dd659d
makhataibar <makhataibar@gmail.com>
2018-06-21 04:51:11 +0600

    homework_2

--- src/components/Message/index.js
+++ src/components/Message/index.js
@@ -1,1 +1,8 @@
-export { default } from './Message';
+import React, { Component } from 'react';
+import './Message.css';
+
+export default class Message extends Component {
+  render() {
+    return <div className="message">{this.props.text}</div>;
+  }
+}
