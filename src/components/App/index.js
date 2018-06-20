e8563b39670b5ace47ece29f2930682fd40ee113
Ivan Opanasiuk <vanjoker88@gmail.com>
2018-06-20 12:32:45 +0300

    done first homework

--- src/components/App/index.js
+++ src/components/App/index.js
@@ -1,1 +1,15 @@
-export { default } from './App';
+import React from 'react';
+
+export default class App extends React.Component {
+    render() {
+        return (
+            <div className='App'>
+                <p className='description'>
+                    Миру-мир, студентам - beer.
+                </p>
+            </div>
+        )
+    }
+}
+
+// export { default } from './App';
