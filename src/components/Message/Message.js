10f3f1b7308b6c5976c6877d90e9cac5fb7355c0
Fedorov Max <fedorov-max89@mail.ru>
2018-06-21 00:24:56 +0300

    HW2 pretier

--- src/components/Message/Message.js
+++ src/components/Message/Message.js
@@ -1,11 +1,9 @@
-import React, {Component} from 'react';
+import React, { Component } from 'react';
 
 export default class Message extends Component {
-    render() {
-        const text = this.props.text;
+  render() {
+    const text = this.props.text;
 
-        return (
-            <span className="message">{text}</span>
-        )
-    }
+    return <span className="message">{text}</span>;
+  }
 }
