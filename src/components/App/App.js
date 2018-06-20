8f05d456cbc82f8cd780b70058ae77c5bac2af9e
flightik <36951757+flightik@users.noreply.github.com>
2018-06-20 18:49:21 +0300

    имя функции с маленькой буквы

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,6 +1,6 @@
 import React from 'react';
 
-export default function First() {
+export default function first() {
     return(
       <div className="App">
         <p className="description">Миру-мир, студентам - beer.</p>
