9cfdeec4eefabaacf6d545a252a68bb8c35fc078
Ivan Zagrebelnyy <ivanzagrebelnyy@gmail.com>
2018-06-20 20:29:24 -0400

    first DZ

---
+++ src/components/App/App.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <p className="description">Миру-мир, студентам - beer.</p>
+      </div>
+    );
+  }
+}
+
+export default App;
