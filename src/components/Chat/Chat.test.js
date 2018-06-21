f9e09fad1d1a598bc13116c898115f445a920f01
Drillibit <293344asd@gmail.com>
2018-06-21 19:33:57 +0600

    homework_2 done

--- src/components/Chat/Chat.test.js
+++ src/components/Chat/Chat.test.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import Chat from '../Chat';
+import Chat from '../Chat/Chat';
 import Message from '../Message';
 import { shallow } from 'enzyme';
 
---
+++ src/components/Message/Message.js
@@ -0,0 +1,5 @@
+import React from 'react';
+
+const Message = (props) => <span className="message">{props.text}</span>;
+
+export default Message; 
