c36488693d7ba8d66af92d7e3e79f4259d97ae59
makhataibar <makhataibar@gmail.com>
2018-06-21 13:26:13 +0600

    add best practice

--- src/components/Message/index.js
+++ src/components/Message/index.js
@@ -1,8 +1,1 @@
-import React, { Component } from 'react';
-import './Message.css';
-
-export default class Message extends Component {
-  render() {
-    return <span className="message">{this.props.text}</span>;
-  }
-}
+export { default } from './Message';
