0b0cb48ac2cdacb9d884d1dc77b215d0e6526983
makhataibar <makhataibar@gmail.com>
2018-06-21 12:36:12 +0600

    fix check-prettier

--- src/components/Message/index.js
+++ src/components/Message/index.js
@@ -1,5 +1,5 @@
-import React, { Component } from "react";
-import "./Message.css";
+import React, { Component } from 'react';
+import './Message.css';
 
 export default class Message extends Component {
   render() {
