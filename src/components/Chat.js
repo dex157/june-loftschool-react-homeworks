21177902706e9041ea1c9d40bb18407e88a73798
Evgeniy Andrusenko <evgeniyandrusenko@gmail.com>
2018-06-22 20:54:27 +0300

    dz 2 done

---
+++ src/components/Chat.js
@@ -0,0 +1,46 @@
+import React, {Component} from 'react';
+import Message from './Message/Message';
+import './Chat/Chat.css';
+
+class Chat extends Component {
+	state = {
+		messageInput: '',
+		messages: []
+	}
+
+	changeInputMessage = event =>{
+		this.setState({messageInput: event.target.value});
+	}
+
+	sendMessageOnEnter = event => {
+		if(event.key === 'Enter'){
+			if(this.state.messageInput !== ''){
+				this.setState({messageInput: '', messages: [...this.state.messages, {text: this.state.messageInput}]});
+			}
+			setTimeout(function(){
+				document.querySelector(".message-list").scrollTop = 5000;
+			},5);
+		}
+	}
+
+	showMessages = () =>{
+		return (
+			<div className="messages">
+				{
+					this.state.messages.map((text, index) => (<Message key={index} text={text.text} />))
+				}
+			</div> 
+		);
+	}
+
+	render(){
+		return (
+			<div className="chat">
+				<div className="message-list">{this.showMessages()}</div>
+				<input onKeyPress={this.sendMessageOnEnter} onChange={this.changeInputMessage} value={this.state.messageInput} className="input-message" placeholder="Введите сообщение..."/>
+			</div>
+		);
+	}
+}
+
+export default Chat;
