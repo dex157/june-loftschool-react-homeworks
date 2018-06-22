6c11b48b2c0914c50795d4d5c706224a0b992adb
mihailKuzmintsev <kuzma.mishka@gmail.com>
2018-06-22 15:51:29 +0300

    complete second homework

---
+++ src/components/Message/Message.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+
+import './Message.css'
+
+class Message extends Component {
+  state = {};
+  render() {
+    return <span className="message">{this.props.text}</span>;
+  }
+}
+
+export default Message;
