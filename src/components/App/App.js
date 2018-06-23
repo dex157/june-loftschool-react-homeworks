2ddebb5114a89c53382cdfc4b5ce3a87d7c7ef30
Vadim Veksler <reyzele91@gmail.com>
2018-06-23 15:22:15 +0300

    first homework done

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,13 +1,11 @@
-import React, {Component} from 'react'
+import React from 'react';
 
-class App extends Component{
-    render(){
-        return(
-            <div className="App">
-                <p className="description">Миру-мир, студентам - beer.</p>
-            </div>
-        );
-    }
+export default class App extends React.Component {
+  render() {
+    return (
+      <div className="App">
+        <p className="description">Миру-мир, студентам - beer.</p>
+      </div>
+    );
+  }
 }
-
-export default App
