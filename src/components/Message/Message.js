cdab2df207fcda648cdc059da39c49404fea4d5d
Anton Lantukh <lantukh@bk.ru>
2018-06-24 00:45:20 +0300

    Подготовил второе задание

---
+++ src/components/Message/Message.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import './Message.css'
+
+class Message extends Component {
+    render() {
+        return (
+            <span className='message'>
+                {this.props.text}
+            </span>
+        )
+    }
+}
+
+export default Message;
