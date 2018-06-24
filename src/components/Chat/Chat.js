e5f2b13d8823220f4c2d879b684a4a2ad6fe485b
jurijsgrabovskis <jurijs.grabovskis@corp.mail.ru>
2018-06-24 16:39:23 +0300

    ДЗ #2 : фиксы по ревью

---
+++ src/components/Chat/Chat.js
@@ -0,0 +1,52 @@
+import React, { Component } from 'react';
+import Message from '../Message/Message';
+import './Chat.css';
+
+class Chat extends Component {
+  state = { messageInput: '', messages: [] };
+
+  changeInputMessage = event => {
+    let inputValue = event.target.value;
+    this.setState({ messageInput: inputValue });
+  };
+
+  sendMessageOnEnter = event => {
+    if (event.charCode === 13) {
+
+      let self = this;
+
+      this.setState(prevState => ({
+        messages: [...prevState.messages, { text: prevState.messageInput }],
+        messageInput: ''
+      }));
+
+      setTimeout(function() {
+        console.log(self.state);
+      }, 250);
+
+      event.target.value = '';
+    }
+
+  };
+
+  render() {
+    return (
+      <div className="chat">
+        <input
+          className="input-message"
+          onKeyPress={this.sendMessageOnEnter}
+          onChange={this.changeInputMessage}
+        />
+        <br />
+        <br />
+        {this.state.messages.map((message, index) => {
+          return (
+            <Message key={message.text + '_' + index} text={message.text} />
+          );
+        })}
+      </div>
+    );
+  }
+}
+
+export default Chat;
--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import Chat from 'components/Chat';
+import Chat from 'components/Chat/Chat';
 import './index.css';
 
 ReactDOM.render(<Chat />, document.getElementById('root'));
