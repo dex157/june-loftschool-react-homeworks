73ca35d4277322c363051381bc2bb411d9c52b63
Evgeniy Andrusenko <evgeniyandrusenko@gmail.com>
2018-06-24 15:26:50 +0300

    prettier

--- src/components/Chat.js
+++ src/components/Chat.js
@@ -1,4 +1,4 @@
-import React, {Component} from 'react';
+import React, { Component } from 'react';
 import Message from './Message/Message';
 import './Chat/Chat.css';
 
@@ -6,38 +6,50 @@ class Chat extends Component {
 	state = {
 		messageInput: '',
 		messages: []
-	}
+	};
 
-	changeInputMessage = event =>{
-		this.setState({messageInput: event.target.value});
-	}
+	changeInputMessage = event => {
+		this.setState({ messageInput: event.target.value });
+	};
 
 	sendMessageOnEnter = event => {
-		if(event.key === 'Enter'){
-			if(this.state.messageInput !== ''){
-				this.setState({messageInput: '', messages: [...this.state.messages, {text: this.state.messageInput}]});
+		if (event.key === 'Enter') {
+			if (this.state.messageInput !== '') {
+				this.setState({
+					messageInput: '',
+					messages: [
+						...this.state.messages,
+						{ text: this.state.messageInput }
+					]
+				});
 			}
-			setTimeout(function(){
-				document.querySelector(".message-list").scrollTop = 5000;
-			},5);
+			setTimeout(function() {
+				document.querySelector('.message-list').scrollTop = 5000;
+			}, 5);
 		}
-	}
+	};
 
-	showMessages = () =>{
+	showMessages = () => {
 		return (
 			<div className="messages">
-				{
-					this.state.messages.map((text, index) => (<Message key={index} text={text.text} />))
-				}
-			</div> 
+				{this.state.messages.map((text, index) => (
+					<Message key={index} text={text.text} />
+				))}
+			</div>
 		);
-	}
+	};
 
-	render(){
+	render() {
 		return (
 			<div className="chat">
 				<div className="message-list">{this.showMessages()}</div>
-				<input onKeyPress={this.sendMessageOnEnter} onChange={this.changeInputMessage} value={this.state.messageInput} className="input-message" placeholder="Введите сообщение..."/>
+				<input
+					onKeyPress={this.sendMessageOnEnter}
+					onChange={this.changeInputMessage}
+					value={this.state.messageInput}
+					className="input-message"
+					placeholder="Введите сообщение..."
+				/>
 			</div>
 		);
 	}
--- src/components/Message/Message.js
+++ src/components/Message/Message.js
@@ -1,11 +1,9 @@
-import React, {Component} from 'react';
+import React, { Component } from 'react';
 import './Message.css';
 
 class Message extends Component {
-	render(){
-		return (
-			<span className="message">{this.props.text}</span>
-		);
+	render() {
+		return <span className="message">{this.props.text}</span>;
 	}
 }
 
