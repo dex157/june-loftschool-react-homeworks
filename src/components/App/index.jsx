acb565c62d59ce33374ff9dd98a6f1b3fff62ae1
Andrey Kustov <kustovforwork@gmail.com>
2018-06-25 23:36:41 +0300

    homework_3(2)

--- src/components/App/index.jsx
+++ src/components/App/index.jsx
@@ -1,4 +1,4 @@
-import React, { Component, Fragment } from 'react';
+import React, { Component } from 'react';
 import './App.css';
 import Step from 'components/Step';
 import CardForm from 'components/CardForm';
