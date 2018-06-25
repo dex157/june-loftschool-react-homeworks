5a2c64d49dfbdd7d83a61228a99b8760687a5699
dkrasheninnikov <40366550+dkrasheninnikov@users.noreply.github.com>
2018-06-25 11:47:24 +0300

    homework1 is done

--- src/components/App/App.test.js
+++ src/components/App/App.test.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { shallow } from 'enzyme';
-import App from './App';
+import App from '../App';
 
 const wrapper = shallow(<App />);
 
--- src/components/App/index.js
+++ src/components/App/index.js
@@ -1,1 +1,14 @@
-export { default } from './App';
+import React from 'react';
+
+class App extends React.Component {
+  state = {};
+  render() {
+    return (
+      <div className="App">
+        <p className="description">Миру-мир, студентам - beer.</p>
+      </div>
+    );
+  }
+}
+
+export default App;
