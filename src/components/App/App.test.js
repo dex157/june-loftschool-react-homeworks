3bb4db8d26a52e659ef128ba5dc0f30cc42d5dad
Ivan Opanasiuk <vanjoker88@gmail.com>
2018-06-25 10:09:57 +0300

    fixed task1

--- src/components/App/App.test.js
+++ src/components/App/App.test.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { shallow } from 'enzyme';
-import App from './index';
+import App from './App';
 
 const wrapper = shallow(<App />);
 
--- src/components/App/index.js
+++ src/components/App/index.js
@@ -1,15 +1,1 @@
-import React from 'react';
-
-export default class App extends React.Component {
-    render() {
-        return (
-            <div className='App'>
-                <p className='description'>
-                    Миру-мир, студентам - beer.
-                </p>
-            </div>
-        )
-    }
-}
-
-// export { default } from './App';
+export { default } from './App';
