0f2ddf11eccae8441aac0de66837f6b6aa974bd1
dkrasheninnikov <40366550+dkrasheninnikov@users.noreply.github.com>
2018-06-25 08:54:25 +0300

    second completed homework

---
+++ src/components/Chat/index.js
@@ -0,0 +1,45 @@
+import React, { Component } from 'react';
+import './Chat.css';
+import Message from '../Message';
+class Chat extends Component {
+  state = {
+    messages: [],
+    messageInput: ''
+  };
+  changeInputMessage = event => {
+    this.setState({ messageInput: event.target.value });
+  };
+  sendMessageOnEnter = event => {
+    const value = this.state.messageInput;
+    if (event.key === 'Enter' && value !== '') {
+      this.setState(state => ({
+        messages: [...state.messages, { text: value }]
+      }));
+      this.setState({ messageInput: '' });
+    }
+  };
+  render() {
+    const messages = this.state.messages;
+
+    //{this.state.messages && <Message text={this.state.messages} />}
+    return (
+      <div className="chat">
+        <div className="message-list">
+          <div className="messages">
+            {messages.map((message, key) => (
+              <Message text={message.text} key={key} />
+            ))}
+          </div>
+        </div>
+        <input
+          className="input-message"
+          value={this.state.messageInput}
+          onChange={this.changeInputMessage}
+          onKeyPress={this.sendMessageOnEnter}
+        />
+      </div>
+    );
+  }
+}
+
+export default Chat;
--- src/index.js
+++ src/index.js
@@ -2,5 +2,4 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import Chat from 'components/Chat';
 import './index.css';
-
 ReactDOM.render(<Chat />, document.getElementById('root'));
