1f70c7e9219ed2afbf828446501cd96a712a380b
Daniil.Petrov <daniil.petrov@leomax.ru>
2018-06-25 18:46:46 +0300

    Done

---
+++ src/components/App/App.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+class App extends React.Component {
+
+  render() {
+    return (
+      <div className='App'>
+        <p className="description">Миру-мир, студентам - beer.</p>
+      </div>
+    );
+  }
+}
+
+export default App;
