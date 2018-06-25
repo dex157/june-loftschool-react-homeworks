e0512592a453bd4e014dde8fcf05807be0649121
Andrey Kustov <kustovforwork@gmail.com>
2018-06-25 23:25:51 +0300

    homework_3(1)

--- src/components/CardForm/CardForm.test.js
+++ src/components/CardForm/CardForm.test.js
@@ -11,28 +11,39 @@ describe('Компонент CardForm', () => {
         expect(wrapper.find('.card-form')).toHaveLength(1);
       });
       it('.card-form input[name="cardNumber"]', () => {
-        expect(wrapper.find('.card-form input[name="cardNumber"]')).toHaveLength(1);
+        expect(
+          wrapper.find('.card-form input[name="cardNumber"]')
+        ).toHaveLength(1);
       });
 
       it('onChange input[name="cardNumber"] должен вызывать handleChangeForm', () => {
         const onChangeFormMock = jest.fn();
         const wrapper = shallow(
-          <CardForm onChangeTimeOver={jest.fn()} onChangeForm={onChangeFormMock} />,
+          <CardForm
+            onChangeTimeOver={jest.fn()}
+            onChangeForm={onChangeFormMock}
+          />
         );
 
         wrapper.find('input[name="cardNumber"]').simulate('change', {
-          target: { value: 'test-value', name: 'test-name' },
+          target: { value: 'test-value', name: 'test-name' }
         });
 
         expect(onChangeFormMock).toHaveBeenCalledTimes(1);
-        expect(onChangeFormMock).toHaveBeenLastCalledWith('test-name', 'test-value');
+        expect(onChangeFormMock).toHaveBeenLastCalledWith(
+          'test-name',
+          'test-value'
+        );
       });
     });
 
     describe('Методы класса', () => {
       const onChangeFormMock = jest.fn();
       const wrapper = mount(
-        <CardForm onChangeTimeOver={jest.fn()} onChangeForm={onChangeFormMock} />,
+        <CardForm
+          onChangeTimeOver={jest.fn()}
+          onChangeForm={onChangeFormMock}
+        />
       );
 
       describe('handleChangeForm', () => {
@@ -41,10 +52,13 @@ describe('Компонент CardForm', () => {
         });
         it('При вызове вызывает функцию onChangeForm переданную через props, аргументами name и value становятся значения из event.target', () => {
           wrapper.instance().handleChangeForm({
-            target: { name: 'test-name', value: 'test-value' },
+            target: { name: 'test-name', value: 'test-value' }
           });
           expect(onChangeFormMock).toHaveBeenCalledTimes(1);
-          expect(onChangeFormMock).toHaveBeenLastCalledWith('test-name', 'test-value');
+          expect(onChangeFormMock).toHaveBeenLastCalledWith(
+            'test-name',
+            'test-value'
+          );
         });
       });
       describe('componentWillUnmount', () => {
