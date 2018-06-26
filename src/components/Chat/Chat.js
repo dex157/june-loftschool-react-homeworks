e06a484d89e7947af1c7b0809e1ac6cfdebc01c6
Andrei <dswfcny7@mail.ru>
2018-06-26 17:14:11 +0300

    Первый вариант

---
+++ src/components/Chat/Chat.js
@@ -0,0 +1,47 @@
+import React, { Component } from "react";
+import Message from "../Message";
+import "./Chat.css";
+
+export default class Chat extends Component {
+  state = {
+    messages: [],
+    messageInput: ""
+  };
+
+  render() {
+    const messages = this.state.messages.map(message =>
+      <Message
+        text={message.text}
+      />);
+
+    return <div className="chat">
+      <div className="message-list">
+        <div className="messages">
+          {messages}
+        </div>
+      </div>
+      <input className="input-message"
+             value={this.state.messageInput}
+             onChange={this.changeInputMessage}
+             onKeyPress={this.sendMessageOnEnter}
+      />
+    </div>;
+  }
+
+  changeInputMessage = event => {
+    this.setState({
+      messageInput: event.target.value
+    });
+  };
+
+  sendMessageOnEnter = event => {
+    if (event.key !== "Enter" || !this.state.messageInput) {
+      return;
+    }
+
+    this.setState({
+      messages: [...this.state.messages, {"text": this.state.messageInput}],
+      messageInput: ""
+    });
+  };
+}
