1e768465e24b8cea381e4f879d6628a5dc050774
Roman Chesnokov <roman.chesnokov@gmail.com>
2018-06-26 23:18:47 +0300

    Homework 4

--- src/components/ModalButton/Modal.js
+++ src/components/ModalButton/Modal.js
@@ -1,8 +1,12 @@
+import ReactDOM from 'react-dom';
 import { Component } from 'react';
 
 class Modal extends Component {
   render() {
-    return null;
+    return ReactDOM.createPortal(
+      this.props.children,
+      document.getElementById('portal')
+    );
   }
 }
 
