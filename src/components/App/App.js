4214332583323acd50703f5d6b1eb51bb776e908
Anton Lantukh <lantukh@bk.ru>
2018-06-29 02:01:54 +0300

    Начало работы над заданием

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,13 +1,25 @@
 import React, { PureComponent } from 'react';
-import { Switch, withRouter } from 'react-router-dom';
+import { Route, Link, Redirect, Switch, withRouter } from 'react-router-dom';
 import { AuthorizeProvider } from 'components/AuthorizeProvider';
+import { Public } from 'components/Public';
+import { Login } from 'components/Login';
+import { PrivateRoute } from 'components/PrivateRoute';
+import { Private } from 'components/Private';
 
 export class App extends PureComponent {
   render() {
     return (
       <AuthorizeProvider>
         <div>
-          <Switch />
+          <Link to="/public">Public</Link>
+          <Link to="/private">Private</Link>
+          <Link to="/login">Login</Link>
+          <Switch>
+            <Route path="/public" component={Public} />
+            <Route path="/login" component={Login} />
+            <PrivateRoute path="/private" component={() => Private} />
+            <Redirect to="/public" />
+          </Switch>
         </div>
       </AuthorizeProvider>
     );
