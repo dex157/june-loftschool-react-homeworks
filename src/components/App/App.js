4e90e395e46d5f6a12d25585cf4b5bcd71ec170e
Irina Kondrateva <irakonrdratyeva@gmail.com>
2018-06-29 19:15:00 +0300

    homework 5

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,20 +1,43 @@
 import React, { PureComponent } from 'react';
-import { Switch, withRouter } from 'react-router-dom';
+import { Switch, Link, Route, withRouter, Redirect } from 'react-router-dom';
 import { AuthorizeProvider } from 'components/AuthorizeProvider';
+import Public from 'components/Public';
+import Private from 'components/Private';
+import Login from 'components/Login';
+import PrivateRoute from '../PrivateRoute/PrivateRoute';
 
 export class App extends PureComponent {
   render() {
     return (
       <AuthorizeProvider>
         <div>
-          <Switch />
+          <nav>
+            <ul>
+              <li>
+                <Link to="/login">Войти</Link>
+              </li>
+              <li>
+                <Link to="/private">Секретная страница</Link>
+              </li>
+              <li>
+                <Link to="/">Главная</Link>
+              </li>
+            </ul>
+          </nav>
+          <Switch>
+            <Route path="/" exact component={Public} />
+            <PrivateRoute path="/private" component={Private} />
+            <Route path="/login" component={Login} />
+            <Redirect to="/" />
+          </Switch>
         </div>
       </AuthorizeProvider>
     );
   }
 }
 
+export default withRouter(App);
+
 // это важно!
 // необходимо использовать этот хок(withRouter), потому что при использовании нескольких контекстов
 // реакт-роутер теряет свой контекст. Причина — использование старого апи.
-export default withRouter(App);
