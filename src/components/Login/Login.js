1c9761e0c1663aba80862f50ada56d4e9a23c1ee
notherkappa <exehot@gmail.com>
2018-06-29 17:52:11 +0300

    homework5

--- src/components/Login/Login.js
+++ src/components/Login/Login.js
@@ -3,15 +3,78 @@ import { AuthHOC } from 'components/AuthorizeProvider';
 import { Redirect } from 'react-router-dom';
 
 class Login extends Component {
+  state = {
+    email: '',
+    password: '',
+    authFailed: false
+  };
   handleChange = e => {
     this.setState({ [e.target.name]: e.target.value });
   };
 
+  login = () => {
+    const { email, password } = this.state;
+    this.setState({ authFailed: !this.props.authorizeUser(email, password) });
+  };
+
   render() {
     const { isAuthorized } = this.props;
+    const { email, password, authFailed } = this.state;
 
-    return isAuthorized ? <Redirect to="/" /> : null;
+    return isAuthorized ? (
+      <Redirect to="/" />
+    ) : (
+      <div>
+        <div>
+          <input name="email" value={email} onChange={this.handleChange} />
+          <input
+            name="password"
+            value={password}
+            onChange={this.handleChange}
+          />
+          {authFailed && <p className="error">Неверный пароль и/или почта.</p>}
+        </div>
+        <button onClick={this.login}>Submit</button>
+      </div>
+    );
   }
 }
 
 export default AuthHOC(Login);
+/*
+import React, { Component, Fragment } from 'react';
+import { AuthHOC } from 'components/AuthorizeProvider';
+import { Redirect } from 'react-router-dom';
+import './login.css';
+
+class Login extends Component {
+  state = {
+    email: '',
+    password: '',
+    failedAuthorize: false
+  };
+
+  handleChange = e => {
+    this.setState({ [e.target.name]: e.target.value });
+  };
+
+  checkLogin = () => {
+    const { email, password } = this.state;
+    const failedAuthorize = !this.props.authorizeUser(email, password);
+
+    if (failedAuthorize) {
+      this.setState({
+        failedAuthorize: true
+      });
+    } else {
+      this.setState({
+        failedAuthorize: false
+      });
+    }
+  };
+
+
+}
+
+export default AuthHOC(Login);
+*/
