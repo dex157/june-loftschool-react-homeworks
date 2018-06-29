acf8a2f8ad31889d890245d685604f2b80fe3fb1
Fedorov Max <fedorov-max89@mail.ru>
2018-06-30 00:55:34 +0300

    HW5 was done

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,13 +1,35 @@
 import React, { PureComponent } from 'react';
-import { Switch, withRouter } from 'react-router-dom';
+import { Switch, withRouter, Link, Route } from 'react-router-dom';
 import { AuthorizeProvider } from 'components/AuthorizeProvider';
+
+import Login from '../Login';
+import PrivateRoute from '../PrivateRoute';
+import Private from '../Private';
+import Public from '../Public';
 
 export class App extends PureComponent {
   render() {
     return (
       <AuthorizeProvider>
+        <nav>
+          <ul>
+            <li>
+              <Link to="/login">Войти</Link>
+            </li>
+            <li>
+              <Link to="/private">Секретная страница</Link>
+            </li>
+            <li>
+              <Link to="/">Главная</Link>
+            </li>
+          </ul>
+        </nav>
         <div>
-          <Switch />
+          <Switch>
+            <Route path="/login" component={Login} />
+            <PrivateRoute path="/private" component={Private} />
+            <Route path="/" component={Public} />
+          </Switch>
         </div>
       </AuthorizeProvider>
     );
