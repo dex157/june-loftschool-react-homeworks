b0188899a72bdb8f9d7fc0fddd9d6478ffd6d42e
Yana Misyuchenko <misyuchenko@uchi.ru>
2018-07-01 22:56:48 +0300

    ready homework_5

--- src/components/AuthorizeProvider/AuthorizeProvider.js
+++ src/components/AuthorizeProvider/AuthorizeProvider.js
@@ -6,6 +6,24 @@ const { Provider, Consumer } = React.createContext({ isAuthorized: false });
 // по этому тут так много кода, изучайте как следует!
 // немного кода я все таки вырезал ^^
 class AuthorizeProvider extends Component {
+  state = {
+    isAuthorized: false
+  };
+
+  validData = {
+    validEmail: 'student',
+    validPassword: '123'
+  };
+
+  authorizeUser = ({ email, password }) => {
+    const { validEmail, validPassword } = this.validData;
+
+    if (email === validEmail && password === validPassword) {
+      this.setState({ isAuthorized: true });
+      return true;
+    } else return false;
+  };
+
   render() {
     const { children } = this.props;
     const { isAuthorized } = this.state;
--- src/components/Login/Login.js
+++ src/components/Login/Login.js
@@ -3,14 +3,46 @@ import { AuthHOC } from 'components/AuthorizeProvider';
 import { Redirect } from 'react-router-dom';
 
 class Login extends Component {
+  state = {
+    email: '',
+    password: '',
+    isAuthSuccess: false
+  };
+
+  handleSubmit = () => {
+    const { authorizeUser } = this.props;
+    const { email, password } = this.state;
+
+    const autorized = authorizeUser({ email, password });
+    this.setState({ isAuthSuccess: autorized });
+  };
+
   handleChange = e => {
     this.setState({ [e.target.name]: e.target.value });
   };
 
   render() {
     const { isAuthorized } = this.props;
+    const { email, password, isAuthSuccess } = this.state;
 
-    return isAuthorized ? <Redirect to="/" /> : null;
+    return isAuthorized ? (
+      <Redirect to="/" />
+    ) : (
+      <div>
+        <div>
+          <input name="email" value={email} onChange={this.handleChange} />
+          <input
+            name="password"
+            value={password}
+            onChange={this.handleChange}
+          />
+          {!isAuthSuccess && (
+            <p className="error"> Неверный пароль и/или почта </p>
+          )}
+        </div>
+        <button onClick={this.handleSubmit}>Submit</button>
+      </div>
+    );
   }
 }
 
