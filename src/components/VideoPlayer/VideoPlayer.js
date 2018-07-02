f6128fdecbce6936ccb27c32130703a1d58e4f0b
Andrey Sergeev <asergeev@avito.ru>
2018-07-03 01:11:54 +0300

    Homework 4

--- src/components/VideoPlayer/VideoPlayer.js
+++ src/components/VideoPlayer/VideoPlayer.js
@@ -3,9 +3,29 @@ import videoFile from './Video.mp4';
 import './VideoPlayer.css';
 
 class VideoPlayer extends PureComponent {
+  video = React.createRef();
+
   render() {
-    return null;
+    return (
+      <div className="video-player">
+        <video className="video-player__source" ref={this.video}>
+          <source src={videoFile} type="video/mp4" />
+        </video>
+        <div>
+          <button onClick={this.handleVideoPlay}>Play</button>
+          <button onClick={this.handleVideoStop}>Stop</button>
+        </div>
+      </div>
+    );
   }
+
+  handleVideoPlay = () => {
+    this.video.current.play();
+  };
+
+  handleVideoStop = () => {
+    this.video.current.pause();
+  };
 }
 
 export default VideoPlayer;
