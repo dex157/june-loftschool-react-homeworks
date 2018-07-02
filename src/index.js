dcd14ef757172660258c60f452f398339353ff18
Andrei <dswfcny7@mail.ru>
2018-07-02 14:55:02 +0300

    +

--- src/index.js
+++ src/index.js
@@ -2,5 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import Chat from './components/Chat';
 import './index.css';
+import Modal from './Modal'
 
 ReactDOM.render(<Chat />, document.getElementById('root'));
+ReactDOM.render(<Modal/>, document.getElementById('root'));
