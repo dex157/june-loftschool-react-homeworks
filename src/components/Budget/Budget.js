ec7c1ec9327de7b7242aba41e517f4fb5175700a
mihailKuzmintsev <kuzma.mishka@gmail.com>
2018-07-03 07:29:29 +0300

    homework 6 done

--- src/components/Budget/Budget.js
+++ src/components/Budget/Budget.js
@@ -1,6 +1,43 @@
 import React from 'react';
 import './Budget.css';
+import { connect } from 'react-redux';
 
-export const Budget = () => <div className="budget" />;
+export const Budget = props => {
+  console.log(props);
+  const { profit, farmExpanse, deliveryExpanse, marketExpanse } = props.budget;
+  console.log(profit, farmExpanse, deliveryExpanse, marketExpanse);
 
-export default Budget;
+  const total = profit - farmExpanse - marketExpanse - deliveryExpanse;
+
+  return (
+    <div className="budget">
+      <h2>Бюджет</h2>
+      <p>
+        {`Всего получено денег: `}
+        <span className="t-profit">{profit}</span>
+      </p>
+      <p>
+        {`Расходы продавцов: `}
+        <span className="t-sellers">{-marketExpanse}</span>
+      </p>
+      <p>
+        {`Расходы на ферме: `}
+        <span className="t-farm">{-farmExpanse}</span>
+      </p>
+      <p>
+        {`Расходы на доставку: `}
+        <span className="t-delivery">{-deliveryExpanse}</span>
+      </p>
+      <p>
+        {`Итого: `}
+        <span className="t-total">{total}</span>
+      </p>
+    </div>
+  );
+};
+
+const mapStateToProps = state => ({
+  budget: state.budget
+});
+
+export default connect(mapStateToProps)(Budget);
