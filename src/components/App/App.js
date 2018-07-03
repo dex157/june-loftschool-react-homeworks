95cb586171a3a090ed807b80cd05a393a452a51a
Drillibit <293344asd@gmail.com>
2018-07-03 20:10:33 +0600

    video display

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,9 +1,11 @@
 import React, { Component } from 'react';
+// import Switcher from '../Switcher';
+import VideoPlayer from '../VideoPlayer';
 import './App.css';
 
 class App extends Component {
   render() {
-    return null;
+    return <VideoPlayer />;
   }
 }
 
