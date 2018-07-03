4094d56bf2d31dd22692eb7d41543d4d6ef6ec8f
Dmitriy Kireev <chief_programmer@mail.ru>
2018-07-03 23:28:18 +0300

    DmitryKireev/homework_5

--- src/components/Login/Login.js
+++ src/components/Login/Login.js
@@ -3,14 +3,51 @@ import { AuthHOC } from 'components/AuthorizeProvider';
 import { Redirect } from 'react-router-dom';
 
 class Login extends Component {
+  state = {
+    email: '',
+    password: '',
+    isAuthSuccess: true
+  };
+
   handleChange = e => {
     this.setState({ [e.target.name]: e.target.value });
   };
 
+  handlerClick = () => {
+    const { authorizeUser } = this.props;
+    const { email, password } = this.state;
+
+    this.setState({ isAuthSuccess: authorizeUser(email, password) });
+  };
+
   render() {
+    const { email, password, isAuthSuccess } = this.state;
     const { isAuthorized } = this.props;
 
-    return isAuthorized ? <Redirect to="/" /> : null;
+    return isAuthorized ? (
+      <Redirect to="/" />
+    ) : (
+      <div>
+        <div>
+          <input
+            name="email"
+            placeholder="login"
+            value={email}
+            onChange={this.handleChange}
+          />
+          <input
+            name="password"
+            placeholder="password"
+            value={password}
+            onChange={this.handleChange}
+          />
+          {!isAuthSuccess && (
+            <p className="error">Неверный пароль и/или почта.</p>
+          )}
+        </div>
+        <button onClick={this.handlerClick}>Submit</button>
+      </div>
+    );
   }
 }
 
