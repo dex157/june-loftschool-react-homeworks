1f54ff6290cca7838a9a030767e858db44f0cf44
szadorozhniy <szadorozhniy@axiomsl.com>
2018-07-03 15:48:38 +0300

    fix cardForm

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -12,38 +12,38 @@ class App extends React.Component {
 		lastName: '',
 		email: '',
 		cardNumber: '',
-        isClickable: false,
-        steps: ['Personal information', 'Card information', 'Finish']
+		isClickable: false,
+		steps: ['Personal information', 'Card information', 'Finish']
 	}
 
 	onChangeForm = (name, value) => {
-        this.setState({[name]: value});
-    }
-    isFormCommitable = () => {
-        const {step, firstName, lastName, email, cardNumber} = this.state;
+			this.setState({[name]: value});
+		}
+		isFormCommitable = () => {
+				const {step, firstName, lastName, email, cardNumber} = this.state;
 
-        if (step === 1) {
-            if (firstName && lastName && email && email.includes('@')) {
-                return true;
-            }
-        } else if (step === 2) {
-            if (cardNumber.length === 16) {
-                return true;
-            }
-        }
-        return false;
-    }
-
-	onChangeFormCard = (event) => {
-		if(event.target.name === 'cardNumber'){
-			this.setState({cardNumber: event.target.value});
-		} 
-		
-		if(this.state.cardNumber.length === 16) {
-			this.setState({isClickable: true});
+				if (step === 1) {
+						if (firstName && lastName && email && email.includes('@')) {
+								return true;
+						}
+				} else if (step === 2) {
+						if (cardNumber.length === 16) {
+								return true;
+						}
+				}
+				return false;
 		}
 
-	}
+	// onChangeFormCard = (event) => {
+	// 	if(event.target.name === 'cardNumber'){
+	// 		this.setState({cardNumber: event.target.value});
+	// 	} 
+		
+	// 	if(this.state.cardNumber.length === 16) {
+	// 		this.setState({isClickable: true});
+	// 	}
+
+	// }
 
 	handleSwitch = () => {
 		const {step} = this.state;
@@ -52,7 +52,7 @@ class App extends React.Component {
 			case 1: 
 				return <PersonalForm firstName={this.state.firstName} lastName={this.state.lastName} email={this.state.email} onChangeForm={this.onChangeForm} />;
 			case 2: 
-				return <CardForm cardNumber={this.state.cardNumber} onChangeFormCard={this.onChangeFormCard}/>;
+				return <CardForm cardNumber={this.state.cardNumber} onChangeForm={this.onChangeForm}/>;
 			case 3: 
 				return <p>Получилось!</p>;
 			default:
@@ -63,15 +63,15 @@ class App extends React.Component {
 
 	handleClickNextForm = () => {
 		const {step} = this.state;
-        this.setState({step: step + 1});
+		this.setState({step: step + 1});
 	}
 
 	handleTabClick = step => {
-        this.setState({step: step});
+		this.setState({step: step});
 	}
 
 	render() {
-        const {steps, step} = this.state;
+		const {steps, step} = this.state;
 		return(
 			<div className='container'>
 				<div className='tab-panel'>
--- src/components/CardForm/CardForm.js
+++ src/components/CardForm/CardForm.js
@@ -1,16 +1,26 @@
-import React from 'react';
+import React, {Component} from 'react';
 import Title from '../Title';
 import './CardForm.css';
 
-function CardForm({cardNumber, onChangeFormCard}) {
+class CardForm extends Component {
 
+	handleChangeForm = e => {
+		const {onChangeForm} = this.props;
+		const {name, value} = e.target;
+
+		onChangeForm(name, value);
+	}
+
+	render(){
+		const {cardNumber} = this.props;
 		return(
 			<div className='card-form'>
 				<Title title='Номер карты'/>
-				<input type="text" name='cardNumber' placeholder="0000000000000000" value={cardNumber} onChange={onChangeFormCard}/>
+				<input type="text" name='cardNumber' placeholder="0000000000000000" value={cardNumber} onChange={this.handleChangeForm}/>
 			</div>
 		)
-
+	}
 }
 
 export default CardForm;
+
