603ae055fd5476ca1f9b914c353b2029d0e34e2f
Yana Misyuchenko <misyuchenko@uchi.ru>
2018-07-04 23:13:57 +0300

    fix travis

--- src/components/Order/Order.js
+++ src/components/Order/Order.js
@@ -5,7 +5,9 @@ const Order = ({ name, price, createdAt }) => (
   <div className="order">
     <div className="order__upper">
       <div className="p--order">Название: {name}</div>
-      <div className="p--order">Цена: {price} </div>
+      <div className="p--order">
+        Цена: <span className="order-price">{price}</span>
+      </div>
     </div>
     <div className="order__lower">
       <div className="p--order">Создан: {createdAt}</div>
