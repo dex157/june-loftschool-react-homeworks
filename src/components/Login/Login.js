2629756175ee1d862d2d124edd75d4e490e1865c
Yerlan <bayekeshov@gmail.com>
2018-07-04 22:06:43 -0400

    Changed all components

--- src/components/Login/Login.js
+++ src/components/Login/Login.js
@@ -1,17 +1,44 @@
-import React, { Component } from 'react';
+import React, { Component, Fragment } from 'react';
 import { AuthHOC } from 'components/AuthorizeProvider';
 import { Redirect } from 'react-router-dom';
 
 class Login extends Component {
+  
+  state = {
+    email: '',
+    password: '',
+    isAuthSuccess: true
+  };
+
   handleChange = e => {
     this.setState({ [e.target.name]: e.target.value });
   };
 
-  render() {
-    const { isAuthorized } = this.props;
+  handlerClick = () => {
+    const { authorizeUser } = this.props, 
+    			{ email, password } = this.state;
 
-    return isAuthorized ? <Redirect to="/" /> : null;
+    this.setState({ 
+    	isAuthSuccess: authorizeUser(email, password) 
+    });
+  };
+
+  render() {
+    const { isAuthorized } = this.props, 
+          { email, password, isAuthSuccess } = this.state;
+
+    return isAuthorized ? (
+      <Redirect to="/private" />
+    ) : (
+      <Fragment>
+          <input name="email" value={email} onChange={this.handleChange} />
+          <input name="password" value={password} onChange={this.handleChange} />
+          {!isAuthSuccess && (<p className="error" style={{color: 'red'}}>Неверный пароль и/или почта.</p>)}
+        	<button onClick={this.handlerClick}>Войти</button>
+      </Fragment>
+    );
   }
 }
+
 
 export default AuthHOC(Login);
