aa4caee973006836016cce4092258506f1d5e6be
Фёдор Кузнецов <kuznetsfedor@gmail.com>
2018-07-05 03:15:43 +0300

    Homework 4 done

--- src/components/CardNumberHolder/CardNumberInput.js
+++ src/components/CardNumberHolder/CardNumberInput.js
@@ -1,9 +1,40 @@
-import React, {Component} from 'react';
+import React, { Component } from 'react';
 
 class CardNumberInput extends Component {
+  format = num => {
+    if (num != null) {
+      num = num.toString();
+      const v = num.replace(/\s+/g, '').replace(/[^0-9]/gi, '');
+      const matches = v.match(/\d{4,}/g);
+      const match = (matches && matches[0]) || '';
+      const parts = [];
+      for (let i = 0, len = match.length; i < len; i += 4) {
+        parts.push(match.substring(i, i + 4));
+      }
+      if (parts.length) {
+        return parts.join(' ');
+      } else {
+        return num;
+      }
+    } else {
+      return (num = '');
+    }
+  };
+
+  normalize = num => {
+    return num.replace(/\s/g, '');
+  };
+
+  componentWillReceiveProps(nextProps) {
+    this.setState({ number: this.format(nextProps.cardNumber) });
+  }
+
+  state = {
+    number: this.format(this.props.cardNumber)
+  };
 
   render() {
-    return null;
+    return <input value={this.state.number} onChange={this.props.onChange} />;
   }
 }
 
--- src/components/Switcher/Switcher.js
+++ src/components/Switcher/Switcher.js
@@ -5,8 +5,48 @@ import './Switcher.css';
 // а так же работать с child.type.name и child.type.displayName
 
 class Switcher extends Component {
+  state = {
+    selectedChild: 0
+  };
+
+  handleChangeChild = event => {
+    this.setState({ selectedChild: event.target.dataset.id });
+  };
+
   render() {
-    return null;
+    const children = React.Children.toArray(this.props.children);
+    return (
+      <div className="switcher">
+        <ul className="component-list">
+          {children.map(
+            (child, index) =>
+              child.type.displayName ? (
+                <li
+                  className="component-list__name"
+                  data-id={index}
+                  key={index}
+                  onClick={this.handleChangeChild}
+                >
+                  {child.type.displayName}
+                </li>
+              ) : (
+                <li
+                  className="component-list__name"
+                  data-id={index}
+                  key={index}
+                  onClick={this.handleChangeChild}
+                >
+                  {child.type.name}
+                </li>
+              )
+          )}
+        </ul>
+        <hr />
+        <div className="component-wrapper">
+          {children[this.state.selectedChild]}
+        </div>
+      </div>
+    );
   }
 }
 
