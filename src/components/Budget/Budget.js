56fa40ab5ef4934b980cdf05ce4f894d3bcdc206
Drillibit <293344asd@gmail.com>
2018-07-06 22:14:51 +0600

    budget reducer and component

--- src/components/Budget/Budget.js
+++ src/components/Budget/Budget.js
@@ -1,6 +1,34 @@
 import React from 'react';
+import { connect } from 'react-redux';
 import './Budget.css';
 
-export const Budget = () => <div className="budget" />;
+export const Budget = ({ market, profit, farm, delivery }) => {
+  const total = market - (profit + farm + delivery);
+  return (
+    <div className="budget">
+      <h2>Бюджет</h2>
+      <p>
+        Всего получено денег<span className="t-profit">{profit}</span>
+      </p>
+      <p>Расходы продавцов</p>
+      <span className="t-sellers">-{market}</span>
+      <p>Расходы на ферме</p>
+      <span className="t-farm">-{farm}</span>
+      <p>Расходы на доставку</p>
+      <span className="t-delivery">-{delivery}</span>
+      <p>Итого</p>
+      <span className="t-total">{total}</span>
+    </div>
+  );
+};
 
-export default Budget;
+const mapStateToProps = state => {
+  return {
+    market: state.budget.marketExpanse,
+    profit: state.budget.profit,
+    farm: state.budget.farmExpanse,
+    delivery: state.budget.deliveryExpanse
+  };
+};
+
+export default connect(mapStateToProps)(Budget);
