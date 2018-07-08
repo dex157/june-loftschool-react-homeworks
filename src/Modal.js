cfd5157c077e85fab047f3728e3805b988cfab1d
Andrei <dswfcny7@mail.ru>
2018-07-08 19:43:12 +0300

    add key

--- src/Modal.js
+++
@@ -1,4 +0,0 @@
-import React from 'react'
-import ReactDom from 'react-dom'
-
-export class PortalExample
--- src/index.js
+++ src/index.js
@@ -2,7 +2,5 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import Chat from './components/Chat';
 import './index.css';
-import Modal from './Modal'
 
 ReactDOM.render(<Chat />, document.getElementById('root'));
-ReactDOM.render(<Modal/>, document.getElementById('root'));
