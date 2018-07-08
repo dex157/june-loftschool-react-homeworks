f2d624b6276e51f5a2ace8bc3804c10d92a525a3
Fedorov Max <fedorov-max89@mail.ru>
2018-07-08 20:38:16 +0300

    ДЗ8 часть 1 Login

--- src/components/Login/Login.js
+++ src/components/Login/Login.js
@@ -1,8 +1,53 @@
 import React, { Component } from 'react';
 
+import './Login.css';
+
 class Login extends Component {
+  state = {
+    authToken: ''
+  };
+
+  handleOnChange = e => {
+    const authToken = e.target.value;
+
+    this.setState({ authToken: authToken });
+  };
+
+  handleKeyPress = e => {
+    switch (e.key) {
+      case 'Enter':
+        if (e.target.value) {
+          console.log('HERE');
+        }
+        break;
+      default:
+        break;
+    }
+  };
+
   render() {
-    return <div>Login</div>;
+    const { authToken } = this.state;
+
+    return (
+      <div className="sc-EHOje dAZrsR">
+        <div className="sc-bZQynM erDCdu">
+          <p>
+            Получить токен нужно на своей странице github, перейдите по{' '}
+            <a href="https://github.com/settings/tokens">адресу</a> и создать
+            себе токен. Запишите куда нибудь токен, так как после создания
+            доступ к нему будет только один раз.
+          </p>
+          <input
+            className="sc-gzVnrw lfdfns"
+            placeholder="auth_token"
+            value={authToken}
+            onKeyPress={this.handleKeyPress}
+            onChange={this.handleOnChange}
+          />
+          <p>После ввода нажать Enter</p>
+        </div>
+      </div>
+    );
   }
 }
 
