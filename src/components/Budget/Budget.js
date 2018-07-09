ff60c7d5a17df78e9b4cb524bf16c0027f56e9d8
Yerlan <bayekeshov@gmail.com>
2018-07-08 21:48:46 -0400

    Added - before each expense

--- src/components/Budget/Budget.js
+++ src/components/Budget/Budget.js
@@ -12,9 +12,9 @@ export const Budget = ({
 	<div className="budget">
 			<h2>Бюджет</h2>
 			<p>Всего получено денег: <span className="t-profit">{profit}</span></p>
-			<p>Расходы продавцов: <span className=".t-sellers">{marketExpanse}</span></p>
-			<p>Расходы на ферме: <span className="t-farm">{farmExpanse}</span></p>
-			<p>Расходы на доставку: <span className="t-delivery">{deliveryExpanse}</span></p>
+			<p>Расходы продавцов: <span className=".t-sellers"> - {marketExpanse}</span></p>
+			<p>Расходы на ферме: <span className="t-farm"> - {farmExpanse}</span></p>
+			<p>Расходы на доставку: <span className="t-delivery"> - {deliveryExpanse}</span></p>
 			<p>Итого: <span className="t-total">{profit - marketExpanse - farmExpanse - deliveryExpanse}</span></p>
 	</div>
 	);
