420bb4373b2b0432083286f7b3d5b3f451170e6f
Yerlan <bayekeshov@gmail.com>
2018-07-08 21:44:16 -0400

    Added t- classes

--- src/components/Budget/Budget.js
+++ src/components/Budget/Budget.js
@@ -11,11 +11,11 @@ export const Budget = ({
 	return (
 	<div className="budget">
 			<h2>Бюджет</h2>
-			<p>Всего получено денег: {profit} </p>
-			<p>Расходы продавцов: {marketExpanse} </p>
-			<p>Расходы на ферме: {farmExpanse} </p>
-			<p>Расходы на доставку: {deliveryExpanse} </p>
-			<p>Итого: {profit - marketExpanse - farmExpanse - deliveryExpanse} </p>
+			<p>Всего получено денег: <span className="t-profit">{profit}</span></p>
+			<p>Расходы продавцов: <span className=".t-sellers">{marketExpanse}</span></p>
+			<p>Расходы на ферме: <span className="t-farm">{farmExpanse}</span></p>
+			<p>Расходы на доставку: <span className="t-delivery">{deliveryExpanse}</span></p>
+			<p>Итого: <span className="t-total">{profit - marketExpanse - farmExpanse - deliveryExpanse}</span></p>
 	</div>
 	);
 }
