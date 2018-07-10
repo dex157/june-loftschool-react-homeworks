48fe8807d2544cb531a96a6773e4e9b21efa4e6a
Drillibit <293344asd@gmail.com>
2018-07-10 14:47:07 +0600

    app component test

---
+++ src/components/AppRouter/AppRouter.js
@@ -0,0 +1,11 @@
+import React, { PureComponent } from 'react';
+import { Switch, Route, withRouter } from 'react-router-dom';
+import './AppRouter.css';
+
+class AppRouter extends PureComponent {
+  render() {
+    return <div className="App">Hello from App</div>;
+  }
+}
+
+export default AppRouter;
