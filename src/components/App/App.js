9422415f48c27e0988b8ad377a9a7fd87dd21f51
Fedor Tolmachev <tolmachevfa@gmail.com>
2018-07-10 19:34:49 +0300

    HW1 done

---
+++ src/components/App/App.js
@@ -0,0 +1,14 @@
+import React, { Component } from "react";
+
+class App extends Component {
+
+    render() {
+        return (
+            <div className="App">
+                <p className="description">Миру-мир, студентам - beer.</p>
+            </div>
+        )
+    }
+}
+
+export default App;
