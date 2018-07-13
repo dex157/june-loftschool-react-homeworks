7ecf70f25c872836badd5f54001244ebc1c6f87f
Vadim Veksler <reyzele91@gmail.com>
2018-07-13 18:53:25 +0300

    .

--- src/components/Budget/Budget.js
+++ src/components/Budget/Budget.js
@@ -13,18 +13,18 @@ class Budget extends React.Component {
           Всего получено денег: <span className="t-profit">{profit}</span>
         </p>
         <p>
-          Расходы продавцов: <span className="t-profit">{-marketExpanse}</span>
+          Расходы продавцов: <span className="t-sellers">{-marketExpanse}</span>
         </p>
         <p>
-          Расходы на ферме: <span className="t-profit">{-farmExpanse}</span>
+          Расходы на ферме: <span className="t-farm">{-farmExpanse}</span>
         </p>
         <p>
           Расходы на доставку:{' '}
-          <span className="t-profit">{-deliveryExpanse}</span>
+          <span className="t-delivery">{-deliveryExpanse}</span>
         </p>
         <p>
           Итого:{' '}
-          <span className="t-profit">
+          <span className="t-total">
             {profit - marketExpanse - farmExpanse - deliveryExpanse}
           </span>
         </p>
