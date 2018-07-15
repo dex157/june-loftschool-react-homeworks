40846d24bc8d45954954d86738ced9b15a524fb0
Ivan Zagrebelnyy <ivanzagrebelnyy@gmail.com>
2018-07-15 17:56:56 -0400

    CardForm works funny

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,9 +1,10 @@
 import React, { Component } from 'react';
 import './App.css';
+import CardNumberHolder from '../CardNumberHolder/CardNumberHolder';
 
 class App extends Component {
   render() {
-    return null;
+    return <CardNumberHolder />;
   }
 }
 
