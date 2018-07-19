7bb59ccbc42c20579c8cc5c087cfab197d4bfc66
dmitry.marokhonov <dmitry.marokhonov@gmail.com>
2018-07-19 09:51:58 +0300

    Домашнее задание 06

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -6,7 +6,11 @@ import Budget from '../Budget';
 
 export class App extends Component {
   render() {
-    return <div className="app" />;
+    return <div className="app">
+      <Market />
+      <Farm />
+      <Budget />
+    </div>;
   }
 }
 
--- src/components/Market/Market.js
+++ src/components/Market/Market.js
@@ -1,4 +1,7 @@
 import React, { Component } from 'react';
+import {connect} from 'react-redux';
+import {createOrder, moveOrderToFarm} from '../../actions/marketActions';
+import Order from '../Order';
 import './Market.css';
 
 let id = 0;
@@ -33,9 +36,64 @@ const getNewOrder = () => {
 };
 
 export class Market extends Component {
+  handleCreateOrderClick = () => {
+    const { onCreateOrder } = this.props;
+    const newOrder = getNewOrder();
+
+    onCreateOrder(newOrder);
+  };
+
+  handleMoveOrderClick = () => {
+    const { orders, onMoveOrderToFarm } = this.props;
+    const order = [...orders].pop();
+    onMoveOrderToFarm(order);
+  };
+
   render() {
-    return <div className="market" />;
+    const { orders } = this.props;
+
+    return (
+      <div className="market">
+        <h2>Новые заказы в магазине</h2>
+
+        <div>
+          <button
+            className="new-orders__create-button"
+            onClick={this.handleCreateOrderClick}
+          >
+            Создать заказ
+          </button>
+
+          <button disabled={!orders.length} onClick={this.handleMoveOrderClick}>
+            Отправить заказ на ферму
+          </button>
+        </div>
+
+        <div className="order-list">
+          {orders.map(order => (
+            <Order
+              key={order.id}
+              name={order.name}
+              price={order.price}
+              createdAt={order.createdAt.toLocaleString()}
+            />
+          ))}
+        </div>
+      </div>
+    );
   }
 }
 
-export default Market;
+const mapStateToProps = state => ({
+  orders: state.market.orders
+});
+
+const mapDispatchToProps = dispatch => ({
+  onCreateOrder: order => dispatch(createOrder(order)),
+  onMoveOrderToFarm: id => dispatch(moveOrderToFarm(id))
+});
+
+export default connect(
+  mapStateToProps,
+  mapDispatchToProps
+)(Market);
