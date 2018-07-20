a8a440b2aa8787406e2e4828750ebd40d56f5e56
dkrasheninnikov <40366550+dkrasheninnikov@users.noreply.github.com>
2018-07-20 21:23:16 +0300

    HW5 is done

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,13 +1,33 @@
 import React, { PureComponent } from 'react';
-import { Switch, withRouter } from 'react-router-dom';
+import { Switch, withRouter, Route, Link } from 'react-router-dom';
 import { AuthorizeProvider } from 'components/AuthorizeProvider';
+import Public from 'components/Public';
+import Login from 'components/Login';
+import PrivateRoute from 'components/PrivateRoute';
 
 export class App extends PureComponent {
   render() {
     return (
       <AuthorizeProvider>
         <div>
-          <Switch />
+          <nav>
+            <ul>
+              <li>
+                <Link to="/login">Войти</Link>
+              </li>
+              <li>
+                <Link to="/private">Секретная страница</Link>
+              </li>
+              <li>
+                <Link to="/">Главная</Link>
+              </li>
+            </ul>
+          </nav>
+          <Switch>
+            <Route path="/" component={Public} exact />
+            <Route path="/login" component={Login} />
+            <Route path="/private" component={PrivateRoute} />
+          </Switch>
         </div>
       </AuthorizeProvider>
     );
--- src/components/Login/Login.js
+++ src/components/Login/Login.js
@@ -3,14 +3,33 @@ import { AuthHOC } from 'components/AuthorizeProvider';
 import { Redirect } from 'react-router-dom';
 
 class Login extends Component {
+  state = {
+    error: false
+  };
   handleChange = e => {
     this.setState({ [e.target.name]: e.target.value });
+    this.setState({ error: false });
   };
-
+  handleClick = e => {
+    const { authorizeUser } = this.props;
+    !authorizeUser(this.state.email, this.state.password) &&
+      this.setState({ error: true });
+  };
   render() {
     const { isAuthorized } = this.props;
-
-    return isAuthorized ? <Redirect to="/" /> : null;
+    const { error } = this.state;
+    return isAuthorized ? (
+      <Redirect to="/private" />
+    ) : (
+      <div>
+        <div>
+          <input name="email" onChange={this.handleChange} />
+          <input name="password" onChange={this.handleChange} type="password" />
+          {error && <p className="error">Неверный пароль и/или почта.</p>}
+        </div>
+        <button onClick={this.handleClick}>Submit</button>
+      </div>
+    );
   }
 }
 
