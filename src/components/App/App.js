6bffff72fdc35e69a7ef093c15e838b8be65ad57
Ivan Zagrebelnyy <ivanzagrebelnyy@gmail.com>
2018-07-23 16:40:35 -0400

    switcher done

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -2,15 +2,16 @@ import React, { Fragment, Component } from 'react';
 import './App.css';
 import CardNumberHolder from '../CardNumberHolder/CardNumberHolder';
 import ModalButton from '../ModalButton/ModalButton';
+import Switcher from '../Switcher/Switcher';
 
 class App extends Component {
   render() {
     return (
-      <Fragment>
+      <Switcher>
         <CardNumberHolder />
         <br />
         <ModalButton />
-      </Fragment>
+      </Switcher>
     );
   }
 }
